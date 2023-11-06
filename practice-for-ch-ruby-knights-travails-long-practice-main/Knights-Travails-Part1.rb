76c9006f7690387474fb89052599a92a0560438f
Yaqi Fang <cfang322@gmail.com>
2023-11-06 14:57:48 -0500

    Started and still modifying phase 2

--- practice-for-ch-ruby-knights-travails-long-practice-main/Knights-Travails-Part1.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/Knights-Travails-Part1.rb
@@ -1,9 +1,8 @@
 class KnightPathFinder
 
     def self.valid_moves(position) #[3, 3]
-        possible_moves = []
         x, y = position
-        possible_moves.push(
+        possible_moves = [
             [x + 2, y + 1],
             [x - 2, y + 1],
             [x + 2, y - 1],
@@ -12,11 +11,10 @@ class KnightPathFinder
             [x - 1, y + 2],
             [x + 1, y - 2],
             [x - 1, y - 2]
-        ) 
-        if @board(position)
-            return true
-        else
-            return false
+        ]
+        # define list of all possible moves within the board
+        valid_moves = possible_moves.select do |move|
+            move.all? { |coord| coord >= 0 && coord < 8 }
         end
     end
 
@@ -30,13 +28,59 @@ class KnightPathFinder
     end 
 
     def new_move_positions(position)
-        kpf = KnightPathFinder.valid_moves(position)
-        if !@considered_positions.include?(position)
-            @considered_positions << position
-        end
+        valid_moves = KnightPathFinder.valid_moves(position)
+        new_positions = valid_moves.reject { |pos| @considered_positions.include?(pos) }
+        @considered_positions += new_positions
+        new_positions
     end
 
-    def build_move_tree(start_position)
-        
+    def build_move_tree(start_pos, n = nil)
+        root = PolyTreeNode.new(@start_position)
+        queue = [root]
+
+        until queue.empty?
+        current_node = queue.shift
+        current_pos = current_node.value
+
+        new_move_positions(current_pos).each do |new_pos|
+            child_node = PolyTreeNode.new(new_pos)
+            current_node.add_child(child_node)
+            queue << child_node
+        end
+
+        root
     end
+    #     (x, y) = start_pos
+    #     start_pos = [x][y]
+    #     valid_moves = []
+    #     queue = deque()
+
+    #     for dx, dy in possible_moves
+    #         new_x, new_y = x + dx, y + dy
+    #         if self.is_valid(new_x, new_y)
+    #             queue.push(new_x, new_y)
+    #             valid_moves.push(new_x, new_y)
+    #         end
+    #     end
+    #     return valid_moves
+    # end 
+
 end 
+#     def build_move_tree(start_position, n = nil) #BFS
+#         @considered_positions = [@start_position] #[0,0]
+#         result = []
+#         #if queue is not empty AND if queue does not already contain a position
+#         if !@considered_positions.empty? && !@considered_positions.include?(@position)
+#             @considered_positions.pop(@start_position)
+#             children 
+#             @considered_positions << @position.children
+#         end
+#     end
+    
+# end 
+
+# [0, 0] <= starting position in que
+# |      \
+# [1,2]  [2,1] 
+# turn ^ into nodes and add to que
+
